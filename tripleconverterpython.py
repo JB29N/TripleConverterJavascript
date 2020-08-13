33d2024a948bb09ba0745c93b374cfe20e52747b
Jean-Baptiste <jean-baptiste.leran@epfl.com>
2020-08-13 13:23:57 +0200

    def integrations

---
+++ tripleconverterpython.py
@@ -0,0 +1,14 @@
+# TRIPLE CONVERTER PYTHON
+
+# Convertion Fonctions
+
+def EuroToCHF(eur)
+    chf = eur*1.08
+
+def KiloToPound(kilo)
+    kilo = pound * 0.4
+
+def LiterToM3(liter)
+    metercube = liter / 1000
+
+# Conversion
